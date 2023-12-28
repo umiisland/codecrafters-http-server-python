ae73ebd45f64a9ee5076cd106ead1f127dc23c39
umiisland <umiisland@hotmail.co.th>
2023-12-28 21:36:00 +0000

    attempt 3rd stage

--- app/main.py
+++ app/main.py
@@ -9,16 +9,31 @@ def main():
     server_socket = socket.create_server((server_host, server_port), reuse_port=True)
     print(f"Server listening on port {server_port}...")
 
-    while True:
-        client_connection_socket, client_addr = server_socket.accept() # wait for client
-        print(f"Accepted connection from {client_addr}")
-        handle_response(client_connection_socket)
+    client_connection_socket, client_addr = server_socket.accept()  # wait for client
+    print(f"Accepted connection from {client_addr}")
+    handle_response(client_connection_socket)
 
 
 def handle_response(client_connection_socket):
-    requst_data = client_connection_socket.recv(1024).decode("utf-8")  # Receive the data from the client
-    response_header = "HTTP/1.1 200 OK\r\n\r\n<p>Hello World!</p>"
-    client_connection_socket.sendall(response_header.encode())  # Sending Response to the client
+    request_data = client_connection_socket.recv(1024).decode("utf-8")  # receive the data from the client
+    path = extract_path(request_data)
+    response_header = ""
+
+    if path == "/":
+        response_header = "HTTP/1.1 200 OK\r\n\r\n"
+        response_body = "<p>Hello World!</p>"
+    else:
+        response_header = "HTTP/1.1 400 Not Found\r\n\r\n"
+        response_body = "<p>Page Not Found!</p>"
+
+    response = response_header + response_body
+    client_connection_socket.sendall(response.encode())  # sending Response to the client
+    print(f"Sent response... \n{response}")
+    client_connection_socket.close()
+
+
+def extract_path(request):
+    return request.split("\r\n")[0].split(" ")[1]
 
 
 if __name__ == "__main__":
